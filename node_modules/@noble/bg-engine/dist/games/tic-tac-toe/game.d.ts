66d0447146730ded714031ef011a87551ff2998b
Ian Noble <inoble@vbasoftware.com>
2026-02-26 19:55:22 -0800

    clean up

--- node_modules/@noble/bg-engine/dist/games/tic-tac-toe/game.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Game } from 'boardgame.io';
-import type { TicTacToeState } from './types.js';
-export declare const TicTacToe: Game<TicTacToeState>;
-//# sourceMappingURL=game.d.ts.map
--- node_modules/@noble/bg-engine/dist/games/tic-tac-toe/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { TicTacToeState } from './types.js';
-export type { CellValue } from './types.js';
-export declare const ticTacToeDef: import("../../index.js").GameDefinition;
-//# sourceMappingURL=index.d.ts.map
