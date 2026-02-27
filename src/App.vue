016bd78912984894bad079c562908aaac0e0ae23
Ian Noble <inoble@vbasoftware.com>
2026-02-27 07:58:41 -0800

    fixed issue with hardcoded server

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { NobleProvider } from "@noble/bg-engine/client";
+
+const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:8000";
 </script>
 
 <template>
-	<NobleProvider server-url="http://localhost:8000" :debug="true">
+	<NobleProvider :server-url="serverUrl" :debug="true">
 		<router-view />
 	</NobleProvider>
 </template>
