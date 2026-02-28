b4bc6380ac2b063020c363b16b88d6e0031d0b6c
Ian Noble <ianoble@gmail.com>
2026-02-27 22:08:06 -0800

    Update @noble/bg-engine/styles import to relative path for Vercel build

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { registerGame } from '../noble-bg-engine/packages/engine/src/client';
 import App from './App.vue';
 import { router } from './router';
 import { gameDef } from './logic/game-logic';
-import '@noble/bg-engine/styles';
+import '../noble-bg-engine/packages/engine/src/styles/core.css';
 import './style.css';
 
 registerGame(gameDef);
